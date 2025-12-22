7a20707f0420d0b5688b23059e6182560042db28
IritB <Irit_seg@hotmail.com>
2025-12-22 19:18:32 +0200

    Add python file

---
+++ 22_12_2025.py
@@ -0,0 +1,64 @@
+
+
+#x = 5
+#print(f"The number is {x}")
+#
+#def new_function():
+#    return "This is a new function."
+#print(new_function())
+#
+#print(type(x))
+#
+#add = 1 + 2
+#print(f"The sum is {add}")
+
+#multiply = 2 * 3
+#print(f"The product price is {multiply}")
+
+#power = 2 ** 3
+#print(f"2 to the power of 3 is {power}")
+
+#divide = 10 / 2
+#print(f"10 divided by 2 is {divide}")
+
+#floor_divide = 9 // 2
+#print(f"9 floor divided by 2 is {floor_divide}")
+
+#modulo = 10 % 3
+#print(f"10 modulo 3 is {modulo}")
+
+#float_test = 0.1 + 0.1 + 0.1
+#print(float_test)
+
+#print(0.3==float_test)
+
+#print(math.isclose(0.3, float_test))
+
+#age = input("Enter your age: ")
+#print(f"You are {age} years old")
+#print(age)
+
+#is_raining = True
+#
+#has_umbrella = False
+#
+#is_wet = is_raining or has_umbrella
+#
+#print(is_wet)
+#
+#str = 'blah lkjdf. lkdf ldfslk. ldkf'
+#
+#str_new = str.replace('.', '.\n')
+#
+#print(str_new)
+#
+#if is_raining and has_umbrella:
+#    print("You stay dry.")
+#elif is_raining and not has_umbrella:
+#    print("You get wet.")
+#else:
+#    print("You stay dry.")
+
+
+
+
