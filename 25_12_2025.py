c6c8c027e4f2c33278f4af8b200af0acfbf6c963
Irit B <irit_seg@hotmail.com>
2025-12-25 18:19:28 +0200

    Add new file 25.12

---
+++ 25_12_2025.py
@@ -0,0 +1,99 @@
+##########
+###LIST###
+##########
+
+# l1 = [1 ,2 , 3]
+
+# print(l1)
+
+# l1.append(4)
+
+# print(l1)
+
+# l1.pop()
+
+# print(l1)
+
+# l1.insert(0, 4)
+
+# print(l1)
+
+# l1.insert(len(l1)-1, 8)
+
+# print(l1)
+
+# l1[len(l1)-1] = "Changed"
+
+# print(l1)   
+
+# l1.remove(1)
+
+# print(l1)
+
+# del l1[0:2]
+
+# print(l1)
+
+# l1 = [1,2,3]
+
+# print(l1)   
+
+# l1.clear()
+
+# print(l1)
+
+# l2 = l1
+
+# l3 = l1.copy()
+
+# print(l2)
+
+# l1.extend([4,5,6]) 
+
+# print(l1)   
+
+# print(l2)   
+
+# print(l3)
+
+
+###########
+###TUPLE###
+###########
+
+# can't be changed
+
+t1 = (1,2,3,3)
+
+print(t1.count(3))
+
+print(t1.index(1))
+
+print(0 in t1)
+
+
+#########
+###SET###
+#########
+
+s1 = {1,2,3,3,4}
+
+s2 = {5,6,7,8,1}
+
+print(s1)   # no duplicates
+
+print(s1.symmetric_difference(s2))
+
+
+################
+###DICTIONARY###
+################
+
+d1 = {"Name": "Alice",
+      "Age": 25}
+
+print(d1)
+
+
+
+
