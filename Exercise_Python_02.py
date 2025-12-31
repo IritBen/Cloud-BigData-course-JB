fe203453b64fd708d085859c62b588c734cf4213
IritB <Irit_seg@hotmail.com>
2025-12-31 16:03:29 +0200

    update file

--- Exercise_Python_02.py
+++ Exercise_Python_02.py
@@ -23,19 +23,53 @@
 
 # 3
 
-def num_of_characters():
-    while True:
-        a = float(input("Insert a number: "))
-        if a == -999:
-            break
-        counter = 1
-        temp = a
-        while abs(temp) >= 10:
-            counter += 1
-            temp = temp / 10
-        print(counter)
+# def num_of_characters():
+#     while True:
+#         a = float(input("Insert a number: "))
+#         if a == -999:
+#             break
+#         counter = 1
+#         temp = a
+#         while abs(temp) >= 10:
+#             counter += 1
+#             temp = temp / 10
+#         print(counter)
         
-num_of_characters()
+# num_of_characters()
+
+# 4
+
+# def divide_change(change):
+#     i = [20, 10, 5, 1]
+#     for num in i:
+#         count = change // num
+#         if count > 0:
+#             sum = count * num
+#             print(f' {count} * {num} = {sum}')
+#             change = change - sum
+
+# divide_change(76)
+
+# 5
+
+def square(a,b):
+    total = a
+    counter = b
+    while counter > 1:
+        total = total * a
+        counter -= 1
+    print(f'the answer is: {total}')
+
+square(2,4)
+
+
+        
+
+
+    
+
+
+
                      
 
 
