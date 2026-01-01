16226227c78c436a80ac58e3a2cd2010da7c109c
IritB <Irit_seg@hotmail.com>
2026-01-01 13:30:00 +0200

    add file 03

--- Exercise_Python_02.py
+++ Exercise_Python_02.py
@@ -76,8 +76,70 @@
 # final_price = discount(2000)
 # print(f'The price after discount is: {final_price}')
 
+# 7
 
-        
+# import math
+
+# def check_if_0(a):
+#     if a != 0:
+#         result1 = True
+#     else:
+#         result1 = False
+#     return result1
+
+# def check_if_greater_or_equal_o(a,b,c):
+#     if ((b*b)- (4*a*c)) >= 0:
+#         result2 = True
+#     else:
+#         result2 = False
+#     return result2
+
+# a = float(input("Enter number for a: "))
+# b = float(input("Enter number for b: "))
+# c = float(input("Enter number for c: "))
+
+# result_1 = check_if_0(a)
+# result_2 = check_if_greater_or_equal_o(a,b,c)
+
+# if result_1 and result_2:
+#     x1 = (-b + math.sqrt((b*b)- (4*a*c))) / 2*a
+#     x2 = (-b - math.sqrt((b*b)- (4*a*c))) / 2*a
+#     print(f'x1 = {x1}')
+#     print(f'x2 = {x2}')
+
+# 8
+
+# a = int(input("Insert first number: "))
+# b = int(input("Insert second number: "))
+
+# menu = {"a" : "The biggest divider", "b" : "The smallest divider", "c" : "The result of pow(a,b)",
+#         "d" : "the result of sqrt(a)-sqrt(b)", "e" : "exit"}
+
+# 9
+
+# import math
+
+# def steps(x,steps):
+#     counter = 1
+#     total = 0
+#     while counter <= steps:
+#         if counter % 2 == 0:
+#             total = total - (pow(x,counter) / math.factorial(counter))
+#         else:
+#             total = total + (pow(x,counter) / math.factorial(counter))
+#         counter += 1
+#     return total
+
+# result = steps(int(input("enter x: ")), int(input("enter steps num: ")))
+
+# print(f" e^x result is: {result}")
+            
+
+
+
+
+
+ 
 
 
     
---
+++ Exercise_Python_03.py
@@ -0,0 +1,56 @@
+# 1
+
+# def is_positive(a):
+#     if a > 0:
+#         print(f"the number {a} is positive")
+#     else:
+#         print(f"the number {a} is negative or equal to 0")
+
+
+# is_positive(5)
+# is_positive(-1)
+
+# 3
+
+# def dynamic_calculation():
+#     a = int(input("Enter first number: "))
+#     b = int(input("Enter second number: ")) 
+#     c = int(input("Enter third number: ")) 
+#     if c == 1:
+#         return a + b
+#     elif c == 2:
+#         return a - b
+#     else:
+#         return ("not perimmted")
+
+# result = dynamic_calculation()
+# print(result)
+
+# 4
+
+# def greetings():
+#     last_name = input("Enter your last name: ")
+#     first_name = input("Enter your first name: ")
+#     gender = input("Enter your gender: ")
+#     if len(gender) == 1 and gender.isalpha():
+#         if gender == "y":
+#             print(f" Hello, Mr. {first_name} {last_name}, NIce to Meet You")
+#         else:
+#             print(f" Hello, Mrs. {first_name} {last_name}, NIce to Meet You")
+#     else:
+#         print("please try again with one character")
+
+# greetings()
+
+# 5
+
+# a = int(input("Enter a number: "))
+
+# if a > 0:
+#     print(f'Abs of {a} is {a}')
+# else:
+#     abs = a * -1
+#     print(f'Abs of {a} is {abs}')
+
+
+    
